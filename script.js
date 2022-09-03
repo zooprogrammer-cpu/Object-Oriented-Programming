b984fcc06184003864adb9b1cc5003db73be822a
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2022-09-02 22:12:24 -0500

    Classes

--- script.js
+++ script.js
@@ -1,51 +1,22 @@
 'use strict';
-//Constructor functions
-const Person = function (firstName, birthYear){
-    //console.log(this);// prints Person empty object
-    this.firstName = firstName
-    this.birthYear = birthYear
+// Class expression
+// const PersonCl = class{
 
-    // Never create a function inside the constructor. 
-    // If we creates 1000s of this Person object,
-    // there would be 1000s of this function, better to 
-    // use prototypes
+// }
 
-    // this.calcAge = function (){
-    //     console.log(2037 - this.birthYear)
-    // }
+// Class declaration
+class PersonCl{
+    constructor(firstName, birthYear){
+        this.firstName = firstName
+        this.birthYear = birthYear
+    }
+
+    calcAge(){
+        console.log(2037 - this.birthYear);
+    }
 }
 
-// only difference between calling the constructor function and 
-// a regular function is we use new
-const jonas = new Person('Jonas', 1991)
+const jessica = new PersonCl('Jessica', 1984)
+console.log(jessica); //prints PersonCl {firstName: "Jessica", birthYear: 1984}
 
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
-
-console.log(jonas); //Person {firstName: "Jonas", birthYear: 1991}
-
-const matilda = new Person('Matilda', 2017); 
-const jack = new Person('Jack', 1975)
-
-// Prototypes
-console.log(Person.prototype);
-Person.prototype.calcAge =function (){
-        console.log(2037 - this.birthYear)
-    }
-// Now we can use calcAge function even if it was not in the object itself
-jonas.calcAge() // prints 46
-matilda.calcAge() // prints 26
-
-console.log(jonas.__proto__)
-console.log(Person.prototype.isPrototypeOf(jonas)) //true
-console.log(Person.prototype.isPrototypeOf(matilda)) //true
-console.log(Person.prototype.isPrototypeOf(Person)) //false
-
-Person.prototype.species = 'Homo Sapiens'
-console.log(jonas); 
-//Person {firstName: "Jonas", birthYear: 1991, calcAge: function, species: "Homo Sapiens"}
-console.log(jonas.hasOwnProperty('firstName'))// true
-console.log(jonas.hasOwnProperty('species'));//false
-
+console.log(jessica.calcAge()); //prints 53
