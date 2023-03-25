ca37589cfecc8582b061af239e25c09245e32204
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-03-24 23:55:01 -0500

    resetting

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//getters and setters. 
-//functions within an object
-const account = {
-    owner: 'Jonas',
-    movements: [200, 300, 400],
+'use strict';
 
-    get latest() {
-        return this.movements.slice(-1).pop()
-    },
-    // not mandatory to have a setter if there is a getter
-    set latest(mov) {
-        return this.movements.push(mov)
-    }
 
-}
-
-console.log(account.latest); //prints 400
-// this would not have worked if there was no get
-// to set a new value, instead of doing account.latest(600)
-// we do this- 
-account.latest = 600
-console.log(account.movements);
- //prints [200, 300, 400, 600]
