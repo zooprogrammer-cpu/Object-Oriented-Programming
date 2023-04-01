5e98f60176b6b6b22fcfeaa7798db59ea1fc2561
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-03-31 20:51:07 -0500

    Getters in Classes

--- script.js
+++ script.js
@@ -17,12 +17,18 @@ class Personcls1 {
     calcAge(){
         console.log(2037 - this.birthYear);
     }
+    // getter in a class
+    get age(){ 
+        return this.birthYear - 30 ; 
+    }
 
 }
 
 const jessica = new Personcls1('Jessica', 1965);
 console.log(jessica);  // prints Personcls1 {firstName: 'Jessica', birthYear: 1956}
 jessica.calcAge(); // prints 72
+// to call the getter in class
+console.log(jessica.age); // prints 1935
 
 // can try adding a method manually
 Personcls1.prototype.greet = function (){
@@ -30,32 +36,12 @@ Personcls1.prototype.greet = function (){
 }
 
 jessica.greet(); // prints Hey Jessica
+// to use a getter - 
+jessica.age; 
 
 // Important points - 
 
 //1. Classes are not hoisted - cannot be used before they are declared 
 //2. Classes are first class citizens - can pass them into functions and return them from functions
 //3. Classes are executed in strict mode
-
-// Getters and Setters
-const account = {
-    name : 'jonas',
-    movements : [100,200,300],
-
-    get latest(){
-        return this.movements.slice(-1).pop(); 
-    },
-    // note the comma before
-    // every setter needs exactly one parameter
-    set latest(mov){
-        this.movements.push(mov); 
-
-    }
-}
-
-// to call the getter, you use it as a property and not like a method
-console.log(account.latest); // prints 300
-// to call a setter, call like a property, not like a method
-account.latest = 50; 
-console.log(account.movements); // prints [100, 200, 300, 50]
 
