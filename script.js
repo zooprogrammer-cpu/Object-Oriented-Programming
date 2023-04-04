3c3cd21122f253235816c3fa11217ba18d958108
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-04-03 23:08:08 -0500

    Class - coding challenge 2

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 'use strict';
 
-// Object.create
-// Least used in the real world
-const personProto = {
-    calcAge(){
-        console.log(2037 - this.birthYear);
-    },
-    init(firstName, birthYear){
-        this.firstName = firstName; 
-        this.birthYear = birthYear; 
+// Challenge 2 - Redo Challenge 1 with Class
+
+class Car {
+    constructor (make, speed){
+        this.make = make; 
+        this.speed = speed; 
+    }
+
+    accelerate(){
+        return console.log('The accelerated speed is: ', this.speed += 10);
+    }
+    brake(){
+        return console.log('The braked speed is:', this.speed -= 20);
+    }
+    // getter to change speed to US miles
+    get speedUS(){
+    return this.speed/ 1.6;
+    }
+
+    //setter- take a new speed and multiply by 1.6
+    set speedUS(speed){
+        this.speed = speed * 1.6; 
     }
 }
 
-const steven = Object.create(personProto);
-console.log(steven);
-steven.name = 'Steve';
-steven.birthYear = 2002;
-steven.calcAge(); // prints 35
-// by adding the init function above, we can make objects programmatically
-const sarah = Object.create(personProto);
-sarah.init('Sarah', 1979);
-sarah.calcAge(); //prints 58 
+let bmw = new Car ('BMW', 120);
+console.log(bmw); //prints Car {make: 'BMW', speed: 120}
+console.log(bmw.speedUS); //prints 75
+bmw.accelerate(); //prints The accelerated speed is: 130
+bmw.brake(); // prints The braked speed is: 110
+//setter - 
+bmw.speedUS = 50;
+console.log(bmw); //prints Car {make: 'BMW', speed: 80}
+//which is 50 * 1.6  = 80
+
 
 
 
