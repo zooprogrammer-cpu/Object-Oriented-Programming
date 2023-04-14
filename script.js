9f0a081cbf5d428bca986d2c6d327351850c12fb
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-04-14 04:17:09 -0500

    Encapsulation

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 'use strict';
 // Another Class Example
 class Account {
-    constructor(owner, currency, pin){
+    constructor(owner, currency, _pin){
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin; 
-        this.movements = []; 
+        this._pin = _pin; 
+        // protected property 
+        this._movements = []; 
         this.locale = navigator.language;
         console.log(`Thanks for opening an Account, ${this.owner}`);
     }
     //Public interface
+    getMovements(){
+        return this._movements; 
+    }
+
     deposit(val){
-        this.movements.push(val);
+        this._movements.push(val);
     }
 
     withdrawl(val){
         this.deposit(-val);
     }
 
-    approveLoan(val){
+    _approveLoan(val){
         return true; 
     }
 
     requestLoan(val){
-        if (this.approveLoan(val)){
+        if (this._approveLoan(val)){
             this.deposit(val);
             console.log(`Loan approved`)
         }
@@ -32,14 +37,24 @@ class Account {
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 console.log(acc1);
-
+// acc1.movements.push (200);
+// acc1.movements.push(-140);
 acc1.deposit(250);
 acc1.withdrawl(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(5000); 
+acc1._approveLoan(5000); 
 // not a good idea for this method to be accessible to the user
 // it is an internal method
 console.log(acc1);
+
+// Encapsulation
+// Let's protect the movements array. 
+// Let everyone know that is it protected _movements
+// Then developers can use a getMovements method 
+// and know that they are not supposed to set, only get
+console.log(`Movements`, acc1.getMovements());
+
+
 
 
 
