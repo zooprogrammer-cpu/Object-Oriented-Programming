63d5f674e4cd74490b0231961866554324a1f4de
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-04-17 17:37:48 +0545

    use geolocation api

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,19 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+    //   const latitude = postion.coords.latitude; 
+    // even better, we can destructure and use latitude variable directly
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, latitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
